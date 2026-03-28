4f9d07e5da8e01bed8ec12e5cbb2434f2608571d
Yosri <yosrri11@gmail.com>
2026-03-28 15:21:01 +0200

    Show background and system UI in Compose preview
    Update the @Preview annotation for WoofPreview in MainActivity.kt to include showBackground = true and showSystemUi = true. This makes the Compose preview render a background and system bars for more accurate design-time rendering.

--- app/src/main/java/com/example/woof/MainActivity.kt
+++ app/src/main/java/com/example/woof/MainActivity.kt
@@ -142,7 +142,7 @@ fun DogInformation(
 /**
  * Composable that displays what the UI of the app looks like in light theme in the design tab.
  */
-@Preview
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun WoofPreview() {
     WoofTheme(darkTheme = false) {
